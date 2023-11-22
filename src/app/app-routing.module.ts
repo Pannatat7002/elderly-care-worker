a3db43d33de51ae9a2b7d159d6a6870bc1179e9b
Pannatat-Idpass <pannatat@idpassglobal.com>
2023-11-22 13:21:33 +0700

    [route]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,32 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ManualConfigPageComponent } from './manual-config-page/manual-config-page.component';
 import { ForgotPageComponent } from './signin-page/component/forgot-page/forgot-page.component';
 import { WorkScheduleComponent } from './landing-page/component/work-schedule/work-schedule.component';
+import { EmployeeProfileComponent } from './landing-page/component/employee-profile/employee-profile.component';
+import { HomePageComponent } from './landing-page/component/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/signin-page', pathMatch: 'full' },  
+  { path: '', redirectTo: '/signin-page', pathMatch: 'full' },
   { path: 'signin-page', component: SigninPageComponent },
-  { path: 'landing', component: LandingPageComponent },
-  { path: 'manualConfig', component: ManualConfigPageComponent },
-  { path: 'forgot-password', component: ForgotPageComponent },
-  { path: 'work-Schedule', component: WorkScheduleComponent },
+  {
+    path: 'landing', component: LandingPageComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/landing/home'
+      },
+      { path: 'home', component: HomePageComponent },
+      { path: 'works', component: WorkScheduleComponent },
+      { path: 'profile', component: EmployeeProfileComponent },
+      // { path: 'details/:id', component: ProductDetailsComponent }
+    ]
+  },
+  // ,
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: '**', component: PageNotFoundComponent }
+  // { path: 'manualConfig', component: ManualConfigPageComponent },
+  // { path: 'forgot-password', component: ForgotPageComponent },
+  // { path: 'work-Schedule', component: WorkScheduleComponent },
 
   // { path: 'signin-page', component: SigninPageComponent }
 
